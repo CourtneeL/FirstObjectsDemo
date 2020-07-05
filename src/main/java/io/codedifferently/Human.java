96e686b8a72c6e12de0c2b85d9bdd8339039813f
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-04 21:19:44 -0400

    Initial commit

---
+++ src/main/java/io/codedifferently/Human.java
@@ -0,0 +1,58 @@
+package io.codedifferently;
+
+public class Human 
+{
+    private String firstName;
+    private String lastName;
+    static int maxEnergy = 100;
+    private int energy;
+
+    public Human(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.energy = 50;
+    }
+
+    public void eat(Fruit fruit)
+    {
+        if(this.energy + fruit.getEnergy() <= maxEnergy)
+        {
+            this.energy = this.energy + fruit.getEnergy();
+        }
+        else
+        {
+            this.energy = maxEnergy;
+            System.out.println("Energy full!");
+        }
+    }
+    
+    public void work(int hours)
+    {
+        int energySpent = hours * 10;
+
+        if(this.energy - energySpent < 0)
+        {
+            System.out.print("Can not do the work. Energy to low. Eat something to gain energy");
+        }
+        else
+        {
+            this.energy = this.energy - energySpent;
+        }
+    }
+
+    public int getEnergy()
+    {
+        return this.energy;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+}
